e82ab9b757c772def49d4a50ff109ba88313da72
Lukas Pfitscher <75246068+lukaspfitscher@users.noreply.github.com>
2026-02-14 10:05:57 +0000

    update

--- install.sh
+++
@@ -1,9 +0,0 @@
-#change to your OS package manager
-apt update
-install(){ apt install -y "$@" ; }
-
-install python3
-install python3-pip
-install python3-requests
-
-printf "\e[32mLaunch agent with: \e[0mpython3 agent2.py\n"
